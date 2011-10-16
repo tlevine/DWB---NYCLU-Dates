a8ddabbe0eec92c86fd7462dd3384844c99066d5
Thomas Levine <thomas.levine@gmail.com>
2011-10-15 23:03:21 -0400

    days for all years

--- get_days.sh
+++ get_days.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo month,day
-cut -d , -f 4 2009.csv|sed -e '1 d' -e '2,$ s/\([0-9][0-9]\)2009/,\1/'
+echo year,month,day
+for file in *.csv
+  do
+  year=`basename $file .csv`
+  cut -d , -f 4 $file|sed -e 's/^/'$year',/' -e 's/"//g'| sed -e '1 d' -e '2,$ s/\([0-9][0-9]\)'$year'/,\1/'
+done
