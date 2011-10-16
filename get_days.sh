f059cb049182ad84497feb517239242cedc809fe
Thomas Levine <thomas.levine@gmail.com>
2011-10-15 23:51:09 -0400

    unix time

--- get_days.sh
+++ get_days.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-echo year,month,day
+echo year,month,day> dates.csv
 for file in 200*.csv
   do
-  year=`basename $file .csv`> dates.csv
+  year=`basename $file .csv`
   cut -d , -f 4 $file|tr -d '"'|sed -e 's/^/'$year',/' -e '1 d' -e '2,$ s/\([0-9][0-9]\)'$year'/,\1/' >> dates.csv
 done
 
 cat dates.csv| grep -e ',.*,' > dates-cleaner.csv
+
+rm dates-posix.csv
+for d in `head dates-cleaner.csv|tr , -`
+  do date -d $d +%s >> dates-posix.csv
+done
