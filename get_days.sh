b6bb6cb57f84dfb57a76d5ff277b76e2741043aa
Thomas Levine <thomas.levine@gmail.com>
2011-10-15 23:37:57 -0400

    clean dates

--- get_days.sh
+++ get_days.sh
@@ -2,6 +2,8 @@
 echo year,month,day
 for file in 200*.csv
   do
-  year=`basename $file .csv`
-  cut -d , -f 4 $file|tr -d '"'|sed -e 's/^/'$year',/' -e '1 d' -e '2,$ s/\([0-9][0-9]\)'$year'/,\1/'
+  year=`basename $file .csv`> dates.csv
+  cut -d , -f 4 $file|tr -d '"'|sed -e 's/^/'$year',/' -e '1 d' -e '2,$ s/\([0-9][0-9]\)'$year'/,\1/' >> dates.csv
 done
+
+cat dates.csv| grep -e ',.*,' > dates-cleaner.csv
