938584cb767dab311e2a0de71b7c579d6ac52a32
Thomas Levine <thomas.levine@gmail.com>
2011-11-11 12:33:24 -0500

    different datetime formats

--- parse_dates.py
+++ parse_dates.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 def test(year):
-  csv=open('times_head.csv')
+  csv=open('time_format/%d' % year)
   for line in csv:
     print parse_line(line,year)
-    return None
 
 def parse_line(line,year):
   """Parse the date and time from a raw line of
@@ -15,12 +14,30 @@ def parse_line(line,year):
 def parse_date(raw,year):
   if year in (2003,2004):
     return parse_date_0304(raw)
+  if year in (2005,2009,2010):
+    return parse_date_050910(raw)
+  if year==2006:
+    return parse_date_06(raw)
+  if year in (2007,2008):
+    return parse_date_0708(raw)
+  else:
+    return raw
 
 def parse_date_0304(d):
   return d
 
+def parse_date_050910(d):
+  return d
+
+def parse_date_06(d):
+  return d
+
+def parse_date_0708(d):
+  return d
+
 def split_time(dateline,year):
   """Split the time into a dict of year and time."""
+  dateline=dateline.replace('\n','')
   if year in (2003,2004,2005):
     time,date=dateline.split(',')
   else:
@@ -31,4 +48,5 @@ def split_time(dateline,year):
   }
 
 if __name__ == "__main__":
-  test(2003)
+  for year in range(2003,2011):
+    test(year)
