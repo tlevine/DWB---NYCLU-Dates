6b79eadcc3406cf2afcaa552d8f4a23cab49e6ed
Thomas Levine <thomas.levine@gmail.com>
2011-11-11 18:34:31 -0500

    ?

--- plot_time_series.R
+++ plot_time_series.R
@@ -19,7 +19,8 @@ import<-function(){
       dfs[interval][[1]]<-rbind(dfs[interval][[1]],tmp)
     }
     dfs[interval][[1]]$interval<-as.POSIXct(
-       dfs[interval][[1]]$interval,origin='1970-01-01'
+      dfs[interval][[1]]$interval
+    , origin=ISOdatetime(1969,12,31,19,0,0),tz="America/New_York"
     )
     plots[interval][[1]]<-ggplot(
         dfs[interval][[1]],aes(interval,count)
@@ -33,7 +34,7 @@ import<-function(){
 }
 
 plot_sqf<-list(
-  typical_week=function(sqf,year=NA){
+  typical_week=function(sqf,year=NA,nlabels=7){
     if (length(year)==2) {
       years<-as.numeric(strftime(sqf$dfs$hours$interval,format='%Y'))
       sqf$dfs$hours<-subset(
@@ -50,7 +51,7 @@ plot_sqf<-list(
     bar<-sqf$dfs$hours$count 
     baz<-aggregate(bar,list(foo),mean)
     names(baz)<-c('hour','count')
-    labelsep<-seq(1,nrow(baz),nrow(baz)/14)
+    labelsep<-seq(1,nrow(baz),nrow(baz)/nlabels)
     baz$index=1:nrow(baz)
 
     partial_plot<- ggplot(baz,aes(index,count)) +
@@ -90,9 +91,14 @@ go<-function(){
   for (year in 2003:2005){
     print(plot_sqf$typical_week(sqf,year)+ylim(0,1500))
   }
-  print(plot_sqf$typical_week(sqf,c(2006,2010))+ylim(0,200))
+  print(plot_sqf$typical_week(sqf,c(2006,2010),nlabels=14)+ylim(0,200))
   for (year in 2006:2010){
-    print(plot_sqf$typical_week(sqf,year)+ylim(0,200))
+    print(plot_sqf$typical_week(sqf,year,nlabels=14)+ylim(0,200))
   }
   dev.off()
+}
+
+
+checks<-function(){
+  lowdays<-sqf$dfs$days[order(sqf$dfs$days$count,decreasing=T),] 
 }
