7ad48bd10c6e881fa6726c8d093850dfc474e5b5
Thomas Levine <thomas.levine@gmail.com>
2011-11-11 10:15:10 -0500

    nohup map

--- slice-map.sh
+++ slice-map.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-year=$1
+yeardir=$1
 lines_per_file=$2
 
-if [ "" = "$year" ] || [ "" = "$lines_per_file" ]
-  then echo You need to specify both the year and the number of lines per file.
+if [ "" = "$yeardir" ] || [ "" = "$lines_per_file" ]
+  then echo You need to specify both the year directory and the number of lines per output file.
 else
   echo slice
+  sleep 1h
+  echo finished
 fi
 
 function slice {
-  #Make a folder for the year
-  mkdir $year
+  #Make a folder for the yeardir
+  mkdir $yeardir
 
   #Extract the header
-  sed -n '1 p' $year.csv > $year/head.csv
+  sed -n '1 p' $yeardir.csv > $yeardir/head.csv
 
   #Split into many slices
-  split -l $lines_per_file -a 3 -d $year.csv slice_
-  for s in slice_[0-9]*
+  split -l $lines_per_file -a 3 -d $yeardir.csv $yeardir/slice_
+  for s in $yeardir/slice_[0-9]*
     do mv $s $s.csv
   done
 
   #Remove the header row from the first slice
-  sed -i '1 d' slice_000.csv
+  sed -i '1 d' $yeardir/slice_000.csv
 }
--- slice.sh
+++ slice.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #Defaults
-if [ "" = $1 ]
+if [ "" = "$1" ]
   then lines_per_file=10000
 else
   lines_per_file=$1
 fi
  
 #Map
-for csv in *.csv
+for csv in data/*.csv
   do
   year=`basename $csv .csv`
-  nohup ./slice-map.sh $year $lines_per_file > $year.log
+  nohup ./slice-map.sh data/$year $lines_per_file > data/$year.log &
 done
